23d1e6f732616ec8f0a707e44513da8f073a3eb7
a7 <89793569+A7-pro@users.noreply.github.com>
2025-03-05 17:19:40 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
+
 cd $HOME/Cybercode
 rm -rf $HOME/.telegram-cli
+
 install() {
-apt install dnsutils
-rm -rf $HOME/.telegram-cli
-sudo chmod +x tg
-chmod +x Cybercode
-chmod +x ts
-./ts
+    apt update && apt upgrade -y
+    apt install -y dnsutils tmux luarocks screen libreadline-dev libconfig-dev libssl-dev \
+                   lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make \
+                   unzip git redis-server autoconf g++ libjansson-dev libpython-dev \
+                   expat libexpat1-dev upstart-sysv
+
+    sudo chmod +x tg Cybercode ts
+    ./ts
 }
+
 if [ "$1" = "ins" ]; then
-install
+    install
 fi
+
 chmod +x install.sh
 lua start.lua
