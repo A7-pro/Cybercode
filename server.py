b02a5ab412aaa5cbd620d1c505d292e858b5f3e0
a7 <89793569+A7-pro@users.noreply.github.com>
2025-03-07 10:47:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from flask import Flask
 import threading
 import os
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!", 200
+    return "Cybercode Bot is running!", 200
 
 def run_bot():
-    os.system("lua /app/start.lua")
+    while True:
+        os.system("lua /app/start.lua")
+        time.sleep(5)  # إعادة تشغيل البوت إذا توقف
 
 if __name__ == "__main__":
-    # تشغيل البوت في Thread منفصل حتى لا يتوقف السيرفر
-    threading.Thread(target=run_bot).start()
+    # تشغيل البوت في Thread منفصل حتى لا يوقف سيرفر Flask
+    threading.Thread(target=run_bot, daemon=True).start()
     
     # تشغيل سيرفر Flask ليستمع على المنفذ 8080
     app.run(host="0.0.0.0", port=8080)
