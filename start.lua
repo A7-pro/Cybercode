d13b80396e9fa66666aa9b1187d63e65dad0c6f4
a7 <89793569+A7-pro@users.noreply.github.com>
2025-03-07 12:34:40 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,17 +1,19 @@
--- تحميل المكتبات المطلوبة
-local redis = dofile("/app/File_Libs/redis.lua").connect("127.0.0.1", 6379)
-local serpent = dofile("/app/File_Libs/serpent.lua")
-local JSON = dofile("/app/File_Libs/dkjson.lua")
-local json = dofile("/app/File_Libs/JSON.lua")
-local URL = dofile("/app/File_Libs/url.lua")
-local https = require("ssl.https")
-local http = require("socket.http")
+local socket = require("socket")
 
--- تشغيل Redis
-os.execute("redis-server --daemonize yes")
+-- تشغيل البوت في خلفية منفصلة
+local function run_bot()
+  os.execute("lua /app/Cybercode.lua")
+end
 
--- مهلة قصيرة للتأكد من أن Redis يعمل قبل تشغيل البوت
-os.execute("sleep 2")
+local co = coroutine.create(run_bot)
+coroutine.resume(co)
 
--- تشغيل البوت
-os.execute("lua /app/Cybercode.lua")
+-- إنشاء سيرفر ويب بسيط باستخدام LuaSocket على المنفذ 8080
+local server = assert(socket.bind("0.0.0.0", 8080))
+print("HTTP server is listening on port 8080...")
+
+while true do
+  local client = server:accept()
+  client:send("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 2\r\n\r\nOK")
+  client:close()
+end
