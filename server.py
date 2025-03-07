f4e4532129f0a6bb8cc0aa18cc2a9d7d569b8690
a7 <89793569+A7-pro@users.noreply.github.com>
2025-03-07 10:41:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import threading
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +8,12 @@ app = Flask(__name__)
 def home():
     return "Bot is running!", 200
 
+def run_bot():
+    os.system("lua /app/start.lua")
+
 if __name__ == "__main__":
+    # تشغيل البوت في Thread منفصل حتى لا يتوقف السيرفر
+    threading.Thread(target=run_bot).start()
+    
+    # تشغيل سيرفر Flask ليستمع على المنفذ 8080
     app.run(host="0.0.0.0", port=8080)
