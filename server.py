52a800d80be2d0ba9de478fdbc685723eacb4923
a7 <89793569+A7-pro@users.noreply.github.com>
2025-03-07 10:54:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,11 +12,12 @@ def home():
 def run_bot():
     while True:
         os.system("lua /app/start.lua")
-        time.sleep(5)  # إعادة تشغيل البوت إذا توقف
+        time.sleep(5)  # إعادة تشغيل البوت في حال توقف
 
 if __name__ == "__main__":
     # تشغيل البوت في Thread منفصل حتى لا يوقف سيرفر Flask
     threading.Thread(target=run_bot, daemon=True).start()
     
-    # تشغيل سيرفر Flask ليستمع على المنفذ 8080
-    app.run(host="0.0.0.0", port=8080)
+    # تشغيل سيرفر Flask ليستمع على المنفذ المطلوب
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
